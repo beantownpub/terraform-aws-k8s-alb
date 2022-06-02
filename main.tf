49b3815696081af929a4780f8773db6a77726ac3
jonny graves <jonny.graves@hqo.co>
2022-06-02 18:42:59 -0400

    fix healthcheck

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_lb_target_group" "kubernetes_api" {
     unhealthy_threshold = 2
     timeout             = 2
     interval            = 5
-    matcher             = "400"
+    matcher             = "200"
   }
   tags = {
     Name = "kubernetes-api"
